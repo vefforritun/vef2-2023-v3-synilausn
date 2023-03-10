f59fb303128187deecd3c3a8a60a8da1184674b6
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2023-03-10 08:04:35 +0000

    fix: lesa PORT úr env

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(router);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
